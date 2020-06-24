998f168a9e96116af1c5ea56bc2f85f0375f90b0
Nisrine <zbadinisrine@gmail.com>
2020-06-23 19:13:49 -0600

    Works properly now

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn=document.getElementById('btn');
+const color=document.querySelector(".color");
+
+btn.addEventListener('click',function() {
+    const randomNumber=getRndInteger(0,3);
+    document.body.style.backgroundColor=colors[randomNumber];
+    color.textContent=colors[randomNumber]
+    
+})
+
+
+function getRndInteger(min,max) {
+    return Math.floor(Math.random() * (max-min) + min)
+}
